cf88ff6705d216b21ffcf8182a052fdb48170809
gabrielspisso <gabrielspisso@outlook.com>
2021-04-22 19:45:20 -0300

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,106 @@
+export const sales = [
+  {
+    id: 1,
+    region: "North America",
+    country: "USA",
+    city: "New York",
+    amount: 1740,
+    date: "2013/01/06"
+  },
+  {
+    id: 2,
+    region: "North America",
+    country: "USA",
+    city: "Los Angeles",
+    amount: 850,
+    date: "2013/01/13"
+  },
+  {
+    id: 3,
+    region: "North America",
+    country: "USA",
+    city: "Denver",
+    amount: 2235,
+    date: "2013/01/07"
+  },
+  {
+    id: 4,
+    region: "North America",
+    country: "CAN",
+    city: "Vancouver",
+    amount: 1965,
+    date: "2013/01/03"
+  },
+  {
+    id: 5,
+    region: "North America",
+    country: "CAN",
+    city: "Edmonton",
+    amount: 880,
+    date: "2013/01/10"
+  },
+  {
+    id: 6,
+    region: "South America",
+    country: "BRA",
+    city: "Rio de Janeiro",
+    amount: 5260,
+    date: "2013/01/17"
+  },
+  {
+    id: 7,
+    region: "South America",
+    country: "ARG",
+    city: "Buenos Aires",
+    amount: 2790,
+    date: "2013/01/21"
+  },
+  {
+    id: 8,
+    region: "South America",
+    country: "PRY",
+    city: "Asuncion",
+    amount: 3140,
+    date: "2013/01/01"
+  },
+  {
+    id: 9,
+    region: "Europe",
+    country: "GBR",
+    city: "London",
+    amount: 6175,
+    date: "2013/01/24"
+  },
+  {
+    id: 10,
+    region: "Europe",
+    country: "DEU",
+    city: "Berlin",
+    amount: 4575,
+    date: "2013/01/11"
+  },
+  {
+    id: 11,
+    region: "Europe",
+    country: "ESP",
+    city: "Madrid",
+    amount: 3680,
+    date: "2013/01/12"
+  },
+  {
+    id: 640,
+    region: "Africa",
+    country: "EGY",
+    city: "Cairo",
+    amount: 500,
+    date: "2015/05/26"
+  },
+  {
+    id: 641,
+    region: "South America",
+    country: "ARG",
+    city: "Buenos Aires",
+    amount: 780,
+    date: "2015/05/07"
+  }
+];
